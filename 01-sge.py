8b3550688df299859d04884e87f5296c4b3ed109
Richard J. de Borja <richard.deborja@uhnresearch.ca>
2017-07-14 09:34:29 -0400

    added ability to write rendered template to an external file

--- 01-sge.py
+++ 01-sge.py
@@ -4,6 +4,7 @@ from grid.Sge import Sge
 
 hpc = Sge()
 test_template = "sge.bash"
+output = "testoutput.sh"
 context = {
 	"jobname": "Job1234",
 	"logdir": "log",
@@ -12,6 +13,12 @@ context = {
 	"sync": "n",
 	"queue": "short",
 	"command": "ls -la"}
-blah = hpc.render_template(template_filename = test_template, context = context)
-print(blah)
+
+hpc.create_cluster_shell_script(
+	output = output,
+	template_filename = test_template,
+	context = context)
+
+
+# blah = hpc.render_template(template_filename = test_template, context = context)
 hpc.submit_job(jobid="1234")
