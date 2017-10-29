4b05df7731ad3a6aed4fdff431b85fa71c98845e
Richard J. de Borja <richard.deborja@uhnresearch.ca>
2017-10-29 15:31:54 -0400

    test cleanup

--- tests/test_Sge.py
+++ tests/test_Sge.py
@@ -16,7 +16,9 @@ context = {
     "sync": "n",
     "queue": "short",
     "command": "ls -la",
-    "submit": False}
+    "submit": False,
+    "queue": "all.q"
+    }
 
 hpc_output = hpc.create_cluster_shell_script(
     output = output,
@@ -26,10 +28,15 @@ hpc_output = hpc.create_cluster_shell_script(
 pp = pprint.PrettyPrinter(indent=4)
 pp.pprint(hpc_output)
 
-
-filecmp.cmp(output, "tests/test_Sge_expected.sh")
-print(os.getcwd())
-
 class testSgeMethods(unittest.TestCase):
     def testScript(self):
-        self.assertTrue(filecmp.cmp(output, "tests/test_Sge_expected.sh"))
+        expected_test_sge_output = "tests/test_Sge_expected.sh"
+        self.assertTrue(filecmp.cmp(output, expected_test_sge_output))
+
+    def get_jobid_from_submission_test(self, sge_output):
+        sge_output_string = "echo 'Your job 8123354 (\"Job1234\") has been submitted"
+        expected_test_sge_jobid = "8123354"
+        self.assertTrue(
+            hpc.get_jobid_from_submission_output_string(sge_output=sge_output_string) == expected_test_sge_jobid
+            )
+
